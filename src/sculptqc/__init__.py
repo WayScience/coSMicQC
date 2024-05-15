74bd4c201be1fe5e819a528c5c9e1b01585bb191
d33bs <dave.bunten@cuanschutz.edu>
2024-05-15 11:20:32 -0600

    add package dynamic versioning capabilities

--- src/sculptqc/__init__.py
+++ src/sculptqc/__init__.py
@@ -3,3 +3,7 @@ Initialization for scupltqc package
 """
 
 from .analyze import find_outliers
+
+# note: version placeholder is updated during build
+# by poetry-dynamic-versioning.
+__version__ = "0.0.1"
