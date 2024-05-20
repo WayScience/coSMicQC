b63f2db37e3ca822866e1030fcb15e21579f8916
d33bs <dave.bunten@cuanschutz.edu>
2024-05-20 10:05:44 -0600

    add yaml threshold input for find_outliers

--- src/cosmicqc/analyze.py
+++ src/cosmicqc/analyze.py
@@ -3,15 +3,24 @@ Module for detecting various quality control aspects from source data.
 """
 
 import operator
+import pathlib
 from functools import reduce
-from typing import Dict, List
+from typing import Dict, List, Optional, Union
 
 import pandas as pd
+import yaml
 from scipy.stats import zscore as scipy_zscore
+
+DEFAULT_QC_THRESHOLD_FILE = (
+    f"{pathlib.Path(__file__).parent!s}/data/qc_thresholds_default.yml"
+)
 
 
 def find_outliers(
-    df: pd.DataFrame, feature_thresholds: Dict[str, float], metadata_columns: List[str]
+    df: pd.DataFrame,
+    metadata_columns: List[str],
+    feature_thresholds: Union[Dict[str, float], str],
+    feature_thresholds_file: Optional[str] = DEFAULT_QC_THRESHOLD_FILE,
 ) -> pd.DataFrame:
     """
     This function uses z-scoring to format the data for detecting outlier
@@ -23,18 +32,31 @@ def find_outliers(
     Args:
         df: pd.DataFrame
             Data frame with converted output from CytoTable.
+        metadata_columns: List[str]
+            List of metadata columns that should be outputted with the outlier data.
         feature_thresholds: Dict[str, float]
-            Dictionary with the feature name(s) as the key(s) and their assigned
+            One of two options:
+            A dictionary with the feature name(s) as the key(s) and their assigned
             threshold for identifying outliers. Positive int for the threshold
             will detect outliers "above" than the mean, negative int will detect
             outliers "below" the mean.
-        metadata_columns: List[str]
-            List of metadata columns that should be outputted with the outlier data.
+            Or a string which is a named key reference found within
+            the feature_thresholds_file yaml file.
+        feature_thresholds_file: Optional[str] = DEFAULT_QC_THRESHOLD_FILE,
+            An optional feature thresholds file where thresholds may be
+            defined within a file.
 
     Returns:
         pd.DataFrame:
             Outlier data frame for the given conditions.
     """
+
+    if isinstance(feature_thresholds, str):
+        feature_thresholds = read_thresholds_set_from_file(
+            feature_thresholds=feature_thresholds,
+            feature_thresholds_file=feature_thresholds_file,
+        )
+
     # Create z-score columns for each feature to reference during outlier detection
     zscore_columns = {}
     for feature in feature_thresholds:
@@ -71,3 +93,40 @@ def find_outliers(
 
     # Return outliers DataFrame with specified columns
     return outliers_df[columns_to_include]
+
+
+def read_thresholds_set_from_file(
+    feature_thresholds: str, feature_thresholds_file: str
+):
+    """
+    Reads a set of feature thresholds from a specified file.
+
+    This function takes the path to a feature thresholds file and a
+    specific feature threshold string, reads the file, and returns
+    the thresholds set from the file.
+
+    Args:
+        feature_thresholds (str):
+            A string specifying the feature thresholds.
+        feature_thresholds_file (str):
+            The path to the file containing feature thresholds.
+
+    Returns:
+        dict: A dictionary containing the processed feature thresholds.
+
+    Raises:
+        LookupError: If the file does not contain the specified feature_thresholds key.
+    """
+
+    with open(feature_thresholds_file, "r") as file:
+        thresholds = yaml.safe_load(file)
+
+    if feature_thresholds not in thresholds["thresholds"]:
+        raise LookupError(
+            (
+                f"Unable to find threshold set by name {feature_thresholds}"
+                f" within {feature_thresholds_file}"
+            )
+        )
+
+    return thresholds["thresholds"][feature_thresholds]
