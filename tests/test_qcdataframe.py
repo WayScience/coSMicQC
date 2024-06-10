f3003e1de72a83a281308149acefc29fe4e63683
d33bs <dave.bunten@cuanschutz.edu>
2024-06-10 14:04:54 -0600

    linting

--- tests/test_qcdataframe.py
+++ tests/test_qcdataframe.py
@@ -5,6 +5,7 @@ Tests cosmicqc qcdataframe module
 import pandas as pd
 from cosmicqc.qcdataframe import QCDataFrame
 
+
 def test_qcdataframe_init_with_dataframe(basic_outlier_dataframe: pd.DataFrame):
     """
     Tests QCDataFrame with pd.DataFrame input.
@@ -13,7 +14,8 @@ def test_qcdataframe_init_with_dataframe(basic_outlier_dataframe: pd.DataFrame):
     assert qc_df.reference == "pd.DataFrame"
     assert qc_df.equals(basic_outlier_dataframe)
 
-def test_qcdataframe_init_with_csv(basic_outlier_csv:str):
+
+def test_qcdataframe_init_with_csv(basic_outlier_csv: str):
     """
     Tests QCDataFrame with CSV input.
     """
@@ -22,16 +24,18 @@ def test_qcdataframe_init_with_csv(basic_outlier_csv:str):
     assert qc_df.reference == basic_outlier_csv
     assert qc_df.equals(expected_df)
 
-def test_qcdataframe_init_with_tsv(basic_outlier_tsv:str):
+
+def test_qcdataframe_init_with_tsv(basic_outlier_tsv: str):
     """
     Tests QCDataFrame with TSV input.
     """
     qc_df = QCDataFrame(data=basic_outlier_tsv)
-    expected_df = pd.read_csv(basic_outlier_tsv, delimiter='\t')
+    expected_df = pd.read_csv(basic_outlier_tsv, delimiter="\t")
     assert qc_df.reference == basic_outlier_tsv
     assert qc_df.equals(expected_df)
 
-def test_qcdataframe_init_with_parquet(basic_outlier_parquet:str):
+
+def test_qcdataframe_init_with_parquet(basic_outlier_parquet: str):
     """
     Tests QCDataFrame with TSV input.
     """
@@ -39,4 +43,3 @@ def test_qcdataframe_init_with_parquet(basic_outlier_parquet:str):
     expected_df = pd.read_parquet(basic_outlier_parquet)
     assert qc_df.reference == basic_outlier_parquet
     assert qc_df.equals(expected_df)
-
