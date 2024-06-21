4ad64c92a85180f29c72a290f249a226995c56d0
d33bs <dave.bunten@cuanschutz.edu>
2024-06-21 15:54:20 -0600

    linting

--- tests/test_scdataframe.py
+++ tests/test_scdataframe.py
@@ -123,9 +123,7 @@ def test_show_report(cytotable_CFReT_data_df: pd.DataFrame):
     )
 
     df.show_report(
-        report_path=(
-            report_path := pathlib.Path("cosmicqc_example_report.html")
-        ),
+        report_path=(report_path := pathlib.Path("cosmicqc_example_report.html")),
         auto_open=False,
     )
 
