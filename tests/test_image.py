28dfd8a18a4ce2495e6b8754547494f4499217da
d33bs <dave.bunten@cuanschutz.edu>
2024-08-05 08:51:21 -0600

    fix tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -7,29 +7,31 @@ from PIL import Image
 
 
 def test_is_image_too_dark_with_dark_image(fixture_dark_image: Image):
-    assert is_image_too_dark(fixture_dark_image, threshold=10.0)
+    assert is_image_too_dark(fixture_dark_image, pixel_brightness_threshold=10.0)
 
 
 def test_is_image_too_dark_with_bright_image(fixture_bright_image: Image):
-    assert not is_image_too_dark(fixture_bright_image, threshold=10.0)
+    assert not is_image_too_dark(fixture_bright_image, pixel_brightness_threshold=10.0)
 
 
 def test_is_image_too_dark_with_mid_brightness_image(
     fixture_mid_brightness_image: Image,
 ):
-    assert not is_image_too_dark(fixture_mid_brightness_image, threshold=10.0)
+    assert not is_image_too_dark(
+        fixture_mid_brightness_image, pixel_brightness_threshold=10.0
+    )
 
 
 def test_adjust_image_brightness_with_dark_image(fixture_dark_image: Image):
     adjusted_image = adjust_image_brightness(fixture_dark_image)
     # we expect that image to be too dark (it's all dark, so there's no adjustments)
-    assert is_image_too_dark(adjusted_image, threshold=10.0)
+    assert is_image_too_dark(adjusted_image, pixel_brightness_threshold=10.0)
 
 
 def test_adjust_image_brightness_with_bright_image(fixture_bright_image: Image):
     adjusted_image = adjust_image_brightness(fixture_bright_image)
     # Since the image was already bright, it should remain bright
-    assert not is_image_too_dark(adjusted_image, threshold=10.0)
+    assert not is_image_too_dark(adjusted_image, pixel_brightness_threshold=10.0)
 
 
 def test_adjust_image_brightness_with_mid_brightness_image(
@@ -37,4 +39,4 @@ def test_adjust_image_brightness_with_mid_brightness_image(
 ):
     adjusted_image = adjust_image_brightness(fixture_mid_brightness_image)
     # The image should still not be too dark after adjustment
-    assert not is_image_too_dark(adjusted_image, threshold=10.0)
+    assert not is_image_too_dark(adjusted_image, pixel_brightness_threshold=10.0)
