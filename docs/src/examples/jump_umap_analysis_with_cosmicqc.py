14c33517803e28db53f618ca7d6b6dc3d8191760
d33bs <dave.bunten@cuanschutz.edu>
2024-09-06 15:39:30 -0600

    update reqs; use pycytominer normalize and feature select

--- docs/src/examples/jump_umap_analysis_with_cosmicqc.py
+++ docs/src/examples/jump_umap_analysis_with_cosmicqc.py
@@ -29,12 +29,14 @@ import pathlib
 import shutil
 from typing import List, Union
 
+import duckdb
+import holoviews
 import hvplot.pandas
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import parsl
 import pyarrow as pa
+import pycytominer
 import umap
 from cytotable.convert import convert
 from parsl.config import Config
@@ -110,11 +112,93 @@ df_labeled_outliers[
 # show histograms to help visualize the data
 df_labeled_outliers.show_report();
 
+# subset the data
+df_full_data = pa.Table.from_batches(
+    [next(parquet.ParquetFile("./BR00117012.parquet").iter_batches(batch_size=60000))]
+).to_pandas()
+df_full_data
+
+# normalize the data using pcytominer
+df_pycytominer_normalized = pycytominer.normalize(
+    profiles=df_full_data,
+    features="infer",
+    image_features=False,
+    meta_features="infer",
+    # samples="Metadata_control_type == 'negcon'",
+    # should this be filled out?
+    method="standardize",
+    output_file=(parquet_pycytominer_normalized := "./BR00117012_normalized.parquet"),
+    output_type="parquet",
+)
+df_pycytominer_normalized
+
+# feature select normalized data using pycytominer
+df_pycytominer_feature_selected = pycytominer.feature_select(
+    profiles=parquet_pycytominer_normalized,
+    operation=[
+        "variance_threshold",
+        "correlation_threshold",
+        "blocklist",
+        "drop_na_columns",
+    ],
+    na_cutoff=0,
+    output_file=(
+        parquet_pycytominer_feature_selected := "./BR00117012_feature_select.parquet"
+    ),
+    output_type="parquet",
+)
+df_pycytominer_feature_selected
+
+pd.read_parquet("./BR00117012_feature_select.parquet").columns.tolist()[-1:]
+
+# join feature selected data with outlier data
+# note: we do this to help ascertain outlier data for
+# objects which may no longer include cosmicqc-checked columns
+# (such as Nuclei_AreaShape_Area)
+with duckdb.connect() as ddb:
+    df_feature_selected_with_cqc_outlier_data = ddb.execute(
+        """
+        SELECT
+            feat_select.*,
+            /* select cosmicqc-specific columns */
+            COLUMNS('cqc\..*')
+        
+        FROM read_parquet('./BR00117012_feature_select.parquet') AS feat_select
+        
+        /* join on the cosmicqc labeled dataframe using metadata columns */
+        LEFT JOIN df_labeled_outliers as cqc_outlier_data ON
+            feat_select.Metadata_ImageNumber = cqc_outlier_data.Metadata_ImageNumber
+            AND feat_select.Metadata_ObjectNumber = cqc_outlier_data.Metadata_ObjectNumber
+            AND feat_select.Metadata_Plate = cqc_outlier_data.Metadata_Plate
+            AND feat_select.Metadata_Well =cqc_outlier_data.Metadata_Well
+        """
+    ).df()
+df_feature_selected_with_cqc_outlier_data
+
 
 # +
-def generate_umap(
-    df_input: Union[cosmicqc.CytoDataFrame, pd.DataFrame], cols_metadata: List[str]
-) -> Union[cosmicqc.CytoDataFrame, pd.DataFrame]:
+def generate_umap_embeddings(
+    df_input: pd.DataFrame, cols_metadata: List[str]
+) -> np.ndarray:
+    """
+    Generates UMAP (Uniform Manifold Approximation and Projection)
+    embeddings for a given input dataframe,
+    excluding specified metadata columns.
+
+    Args:
+        df_input (pd.DataFrame]):
+            A dataframe which is expected to contain
+            numeric columns to be used for UMAP fitting.
+        cols_metadata (List[str]):
+            A list of column names representing
+            metadata columns that should be excluded
+            from the UMAP transformation.
+
+    Returns:
+        np.ndarray:
+            A dataframe containing the UMAP embeddings
+            with 2 components for each row in the input.
+    """
 
     # Set constants
     umap_random_seed = 0
@@ -140,52 +224,123 @@ def generate_umap(
     return embeddings
 
 
-df_labeled = cosmicqc.analyze.label_outliers(
-    df=(
-        pa.Table.from_batches(
-            [
-                next(
-                    parquet.ParquetFile("./BR00117012.parquet").iter_batches(
-                        batch_size=60000
-                    )
-                )
-            ]
-        )
-        .to_pandas()
-        .replace([np.inf, -np.inf], np.nan)
-        .pipe(lambda df: df.drop(labels=df.columns[df.isna().any()], axis=1))
-        .reset_index()
-    ),
-    include_threshold_scores=True,
+all_embeddings = generate_umap(
+    df_input=df_feature_selected_with_cqc_outlier_data, cols_metadata=metadata_cols
 )
-
-embeddings = generate_umap(df_input=df_labeled, cols_metadata=metadata_cols)
-embeddings
+all_embeddings
 # -
 
-pd.DataFrame(embeddings).hvplot.scatter(
-    title="UMAP of JUMP dataset",
-    x="0",
-    y="1",
-    alpha=0.1,
-    rasterize=True,
-    cnorm="linear",
-    height=500,
-    width=800,
+type(embeddings)
+
+
+# +
+def plot_hvplot_scatter_general(
+    embeddings: np.ndarray,
+) -> holoviews.core.spaces.DynamicMap:
+    """
+    Creates a generalized scatter hvplot for viewing
+    UMAP embedding data.
+
+    Args:
+        embeddings (np.ndarray]):
+            A numpy ndarray which includes
+            embedding data to display.
+
+    Returns:
+        holoviews.core.spaces.DynamicMap:
+            A dynamic holoviews scatter plot which may be
+            displayed in a Jupyter notebook.
+    """
+    return pd.DataFrame(embeddings).hvplot.scatter(
+        title="UMAP of JUMP dataset",
+        x="0",
+        y="1",
+        alpha=0.1,
+        rasterize=True,
+        cnorm="linear",
+        height=500,
+        width=800,
+    )
+
+
+plot_hvplot_scatter_general(embeddings=embeddings)
+
+
+# +
+def plot_hvplot_scatter_outliers(
+    embeddings: np.ndarray, cosmicqc_outlier_labels: pd.DataFrame, color_column: str
+):
+    """
+    Creates an outlier-focused scatter hvplot for viewing
+    UMAP embedding data with cosmicqc outliers coloration.
+
+    Args:
+        embeddings (np.ndarray]):
+            A numpy ndarray which includes
+            embedding data to display.
+        cosmicqc_outlier_labels (pd.DataFrame):
+            A dataframe which includes cosmicqc outlier
+            data labels for use in coloration of plot.
+        color_column (str):
+            Column name from cosmicqc_outlier_labels to use
+            for coloring the scatter plot.
+
+    Returns:
+        holoviews.core.spaces.DynamicMap:
+            A dynamic holoviews scatter plot which may be
+            displayed in a Jupyter notebook.
+    """
+    return pd.DataFrame(embeddings).hvplot.scatter(
+        title="UMAP of JUMP erroneous outliers",
+        x="0",
+        y="1",
+        alpha=0.1,
+        rasterize=True,
+        c=cosmicqc_outlier_labels[color_column].astype(int).values,
+        cnorm="linear",
+        cmap="plasma",
+        bgcolor="black",
+        height=500,
+        width=800,
+    )
+
+
+plot_hvplot_scatter_outliers(
+    embeddings=embeddings,
+    cosmicqc_outlier_labels=df_feature_selected_with_cqc_outlier_data,
+    color_column="cqc.small_and_low_formfactor_nuclei.is_outlier",
+)
+# -
+
+plot_hvplot_scatter_outliers(
+    embeddings=embeddings,
+    cosmicqc_outlier_labels=df_feature_selected_with_cqc_outlier_data,
+    color_column="cqc.elongated_nuclei.is_outlier",
 )
 
-pd.DataFrame(embeddings).hvplot.scatter(
-    title="UMAP of JUMP erroneous outliers",
-    x="0",
-    y="1",
-    alpha=0.1,
-    rasterize=True,
-    c=df_labeled["cqc.small_and_low_formfactor_nuclei.is_outlier"].astype(int).values,
-    cnorm="linear",
-    cmap="plasma",
-    bgcolor="black",
-    height=500,
-    width=800,
+plot_hvplot_scatter_outliers(
+    embeddings=embeddings,
+    cosmicqc_outlier_labels=df_feature_selected_with_cqc_outlier_data,
+    color_column="cqc.large_nuclei.is_outlier",
 )
+
+with duckdb.connect() as ddb:
+    df_feature_selected_without_cqc_outlier_data = ddb.execute(
+        """
+        SELECT *
+        FROM df_feature_selected_with_cqc_outlier_data AS feat_select_ouliers
+        WHERE feat_select_ouliers."cqc.small_and_low_formfactor_nuclei.is_outlier" = False
+        AND feat_select_ouliers."cqc.large_nuclei.is_outlier" = False
+        AND feat_select_ouliers."cqc.elongated_nuclei.is_outlier" = False
+        """
+    ).df()
+df_feature_selected_without_cqc_outlier_data
+
+embeddings_outliers_removed = generate_umap(
+    df_input=df_feature_selected_without_cqc_outlier_data, cols_metadata=metadata_cols
+)
+embeddings_outliers_removed
+
+plot_hvplot_scatter_general(embeddings=embeddings_outliers_removed)
 
 
